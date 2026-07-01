60df45b34ca2cdfed78e53a2744e39c7ab371bbb
Evan <evan.dev.0214@gmail.com>
2026-07-01 15:28:46 +0800

    chore: make SwaggerSchema response property optional

--- apps/backend/src/@types/swagger.d.ts
+++ apps/backend/src/@types/swagger.d.ts
@@ -3,7 +3,7 @@ import type { SchemaObject } from 'openapi3-ts/oas30';
 declare global {
   type SwaggerSchema = {
     request?: SchemaObject,
-    response: SchemaObject
+    response?: SchemaObject
   };
 }
 
